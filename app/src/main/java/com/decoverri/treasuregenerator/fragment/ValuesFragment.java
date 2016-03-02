b7eaff43467a4b1c73cce308d77c1e96c3e2803b
Deco Verri <decoverri@gmail.com>
2016-03-02 19:36:30 -0300

    adding differences to values screen

--- app/src/main/java/com/decoverri/treasuregenerator/fragment/ValuesFragment.java
+++ app/src/main/java/com/decoverri/treasuregenerator/fragment/ValuesFragment.java
@@ -7,37 +7,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.decoverri.treasuregenerator.R;
 import com.decoverri.treasuregenerator.activity.GeneratorActivity;
 import com.decoverri.treasuregenerator.model.TreasureType;
 import com.decoverri.treasuregenerator.model.TypeValueDTO;
 import com.decoverri.treasuregenerator.task.GenerateTask;
-
-import java.util.ArrayList;
+import com.decoverri.treasuregenerator.viewHelper.ValuesHelper;
 
 /**
  * Created by decoverri on 12/02/16.
  */
 public class ValuesFragment extends Fragment {
 
+    private ValuesHelper helper;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.values, container, false);
+        helper = new ValuesHelper(view, getActivity());
 
         final TreasureType type = (TreasureType) getArguments().getSerializable("selectedType");
 
-        ArrayList<Double> values = type.getValues();
+        helper.fillViews(type);
 
-        View view = inflater.inflate(R.layout.values, container, false);
-
-        ListView listView = (ListView) view.findViewById(R.id.values);
-
-        listView.setAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, values));
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        helper.getValuesView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 GeneratorActivity activity = (GeneratorActivity) getActivity();
---
+++ app/src/main/java/com/decoverri/treasuregenerator/viewHelper/ValuesHelper.java
@@ -0,0 +1,44 @@
+package com.decoverri.treasuregenerator.viewHelper;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.decoverri.treasuregenerator.R;
+import com.decoverri.treasuregenerator.model.TreasureType;
+
+/**
+ * Created by decoverri on 02/03/16.
+ */
+public class ValuesHelper {
+
+    private LinearLayout header;
+    private Activity activity;
+    private TextView nameView;
+    private TextView letterView;
+    private ListView valuesView;
+
+    public ValuesHelper(View view, Activity activity) {
+        this.activity = activity;
+        header = (LinearLayout) view.findViewById(R.id.type_header);
+        nameView = (TextView) view.findViewById(R.id.type_name);
+        letterView = (TextView) view.findViewById(R.id.type_letter);
+        valuesView = (ListView) view.findViewById(R.id.values);
+    }
+
+    public void fillViews(TreasureType type) {
+        header.setBackgroundColor(activity.getResources().getColor(activity.getResources()
+                .getIdentifier("type" + type.getLetter(), "color", activity.getPackageName())));
+
+        nameView.setText(type.getName());
+        letterView.setText("(Type " + type.getLetter() + ")");
+        valuesView.setAdapter(new ArrayAdapter<>(activity, R.layout.value_item, type.getValues()));
+    }
+
+    public ListView getValuesView() {
+        return valuesView;
+    }
+}
